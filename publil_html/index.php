c794492a07280caaf2a6c856f3ba7b888a98079f
Adam Pedroza <apedroza6@cnm.edu>
2017-02-16 10:14:09 -0700

    added finger point to nav bar in index.php

--- publil_html/index.php
+++ publil_html/index.php
@@ -31,7 +31,7 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#">Brand</a>
+						<a class="navbar-brand" href="#"><i class="fa fa-hand-o-right" aria-hidden="true"></i></a>
 					</div>
 
 					<!-- Collect the nav links, forms, and other content for toggling -->
