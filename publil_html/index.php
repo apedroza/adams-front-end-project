47a38c9652404c5c6e0cf68f20d7c76871106214
Adam Pedroza <apedroza6@cnm.edu>
2017-02-16 09:49:03 -0700

    added index.php

---
+++ publil_html/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+	<head lang="en">
+		<meta charset="utf-8"/>
+
+		<link rel="stylesheet" href="style.css">
+		<script src="script.js"></script>
+		<title>Star Trek Plunker</title>
+	</head>
+	<body>
+		<header>
+			<h1 strong>Adam's Star Trek Plunker!</strong/h1>
+		</header>
+
+		<p>These are the voyages of the Starship Enterprise. Its continuing mission, to
+			explore strange new worlds, to seek out new life and new civilizations, to boldly
+			go where no one has gone before. We need to neutralize the homing signal. Each
+			unit has total environmental control, gravity, temperature, atmosphere, light,
+			in a protective field. Sensors show energy readings in your area. We had a forced
+			chamber explosion in the resonator coil. Field strength has increased by 3,000
+			percent.</p>
+
+		<p>We're acquainted with the wormhole phenomenon, but this... Is a remarkable
+			piece of bio-electronic engineering by which I see much of the EM spectrum
+			ranging from heat and infrared through radio waves, et cetera, and forgive me
+			if I've said and listened to this a thousand times. This planet's interior heat
+			provides an abundance of geothermal energy. We need to neutralize the homing
+			signal.</p>
+
+		<p>Sensors indicate human life forms 30 meters below the planet's surface. Stellar
+			flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar
+			006, warp seven. There's no evidence of an advanced communication network. Total
+			guidance system failure, with less than 24 hours' reserve power. Shield effectiveness
+			has been reduced 12 percent. We have covered the area in a spherical pattern which
+			a ship without warp drive could cross in the given time.</p>
+
+	</body>
+</html>
