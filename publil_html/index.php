a3fe321f4a2a89d1b878fefcf42253515ae592f1
Adam Pedroza <apedroza6@cnm.edu>
2017-02-16 10:05:57 -0700

    added nav bar to index.php

--- publil_html/index.php
+++ publil_html/index.php
@@ -21,30 +21,63 @@
 	</head>
 	<body>
 		<header>
-			<h1>Adam's Front End Project!</h1>
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Brand</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+							<li><a href="#">Link</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+									<li><a href="#">Something else here</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Separated link</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">One more separated link</a></li>
+								</ul>
+							</li>
+						</ul>
+						<form class="navbar-form navbar-left">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Search">
+							</div>
+							<button type="submit" class="btn btn-default">Submit</button>
+						</form>
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Link</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+									<li><a href="#">Something else here</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Separated link</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+
 		</header>
 
-		<p>These are the voyages of the Starship Enterprise. Its continuing mission, to
-			explore strange new worlds, to seek out new life and new civilizations, to boldly
-			go where no one has gone before. We need to neutralize the homing signal. Each
-			unit has total environmental control, gravity, temperature, atmosphere, light,
-			in a protective field. Sensors show energy readings in your area. We had a forced
-			chamber explosion in the resonator coil. Field strength has increased by 3,000
-			percent.</p>
-
-		<p>We're acquainted with the wormhole phenomenon, but this... Is a remarkable
-			piece of bio-electronic engineering by which I see much of the EM spectrum
-			ranging from heat and infrared through radio waves, et cetera, and forgive me
-			if I've said and listened to this a thousand times. This planet's interior heat
-			provides an abundance of geothermal energy. We need to neutralize the homing
-			signal.</p>
-
-		<p>Sensors indicate human life forms 30 meters below the planet's surface. Stellar
-			flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar
-			006, warp seven. There's no evidence of an advanced communication network. Total
-			guidance system failure, with less than 24 hours' reserve power. Shield effectiveness
-			has been reduced 12 percent. We have covered the area in a spherical pattern which
-			a ship without warp drive could cross in the given time.</p>
+		<p>
 
 	</body>
 </html>
