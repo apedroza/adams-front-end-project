fffdbde9fd688fe236bf244adf66f456e61f2294
Adam Pedroza <apedroza6@cnm.edu>
2017-02-16 18:40:29 -0700

    Made some changes to page structure making the photos round

--- public_html/index.php
+++ public_html/index.php
@@ -16,12 +16,12 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-		<script src="script.js"></script>
+		<script src="js"></script>
 		<title>Adam's Front End Project</title>
 	</head>
 	<body>
 		<header>
-			<nav class="navbar navbar-default">
+			<nav class="navbar navbar-inverse">
 				<div class="container-fluid">
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<div class="navbar-header">
@@ -43,7 +43,7 @@
 			</nav>
 			<div class="col-xs-10 col-xs-offset-1">
 				<div class="jumbotron">
-					...If We Don't Got it, You Don't Want It
+					...just a little bit
 				</div>
 			</div>
 			<div class="row row-flex">
@@ -52,7 +52,7 @@
 				</div>
 				<div class="col-md-6">
 					<div class="well">
-						<p>While Of Mice and Men occurs in a very specific time and place, each of the
+						<p id="1">While Of Mice and Men occurs in a very specific time and place, each of the
 							characters can be thought of as symbolizing broader populations. Though the book
 							is not an allegory, and each character can stand alone as simply a character,
 							there’s still something to be gained by looking at each character as representative
@@ -60,11 +60,14 @@
 					</div>
 				</div>
 			</div>
+
+
+
 			<div>
 				<div class="row">
 					<div class="col-sm-2 col-md-2 col-sm-offset-2">
 						<div class="thumbnail">
-							<img src="images/city.jpg" alt="city">
+							<img class="img-circle" src="images/city.jpg" alt="city">
 							<div class="Sacramento">
 								<h3>City</h3>
 								<p>Great city . Yeah Yeah..</p>
@@ -73,7 +76,7 @@
 					</div>
 					<div class="col-sm-2 col-md-2">
 						<div class="thumbnail">
-							<img src="images/tacos.jpg" alt="tacos">
+							<img class="img-circle" src="images/tacos.jpg" alt="tacos">
 							<div class="Tacos">
 								<h3>Tacos</h3>
 								<p>Mmmm. Best Tacos.</p>
@@ -82,16 +85,16 @@
 					</div>
 					<div class="col-sm-2 col-md-2">
 						<div class="thumbnail">
-							<img src="images/tyson.jpg" alt="tyson">
+							<img class="img-circle" src="images/tyson.jpg" alt="tyson">
 							<div class="Mike Tyson">
 								<h3>Mike Tyson</h3>
-								<p>I Love Pigeonss!!</p>
+								<p>I Love Pigeons!!</p>
 							</div>
 						</div>
 					</div>
 					<div class="col-sm-2 col-md-2">
 						<div class="thumbnail">
-							<img src="images/startrek.jpg" alt="trek">
+							<img class="img-circle" src="images/startrek.jpg" alt="trek">
 							<div class="Star Trek Ship">
 								<h3>Star Trek</h3>
 								<p>Trek Ship. WOW!!</p>
@@ -103,16 +106,12 @@
 			<footer>
 				<div class="container">
 					<div class="row">
-						<div class="col-md-6 col-md-offset-5">
+						<div class="col-md-7 col-md-offset-5">
 							&copy; 2017 Roza Productions 2017
 						</div>
 					</div>
 				</div>
 			</footer>
-
 		</header>
-
-		<p>
-
 	</body>
 </html>
