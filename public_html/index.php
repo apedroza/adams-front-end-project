ce035c87e755a99eb6d89bbb1ffa1e1db2c83aa2
Adam Pedroza <apedroza6@cnm.edu>
2017-02-16 23:45:48 -0700

    cleaned up alignment

--- public_html/index.php
+++ public_html/index.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
-<html>
-
-	<head lang="en">
+<html lang="en">
+	<head>
 		<meta charset="utf-8"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
 		<!-- Font Awesome -->
 		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" />
 		<link rel="stylesheet" href="style.css">
+
 		<!-- jQuery -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<script src="js"></script>
@@ -41,79 +45,88 @@
 								<li><a href="#">Link</a></li>
 							</ul>
 						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
 
 						<div class="col-xs-10 col-xs-offset-1">
 							<div class="jumbotron">
-								...just a little bit
+								<h1>HELLO!! ADAM'S FIRST BOOTSTRAP</h1>
 							</div>
 						</div>
-					</div><!-- /.container-fluid -->
-				</nav>
 			</header>
 
-		<main>
-			<div class="container">
-				<!-- row 1-->
-				<div class="row">
-					<div class="col-sm-2 col-md-offset-2">
-						<img src="images/lenny.jpg" alt="lenny">
+			<main>
+				<div class="container">
+					<!--row 1-->
+					<div class="row">
+						<div class="col-xs-12">
+						</div>
 					</div>
-					<div class="col-md-6 col-md-offset-2">
-						<p id="1">While Of Mice and Men occurs in a very specific time and place, each of the
+
+					<!-- row 2-->
+					<div class="row">
+						<div class="col-sm-2 col-md-offset-2">
+							<img src="images/lenny.jpg" alt="lenny">
+						</div>
+						<div class="col-md-6 col-md-offset-2">
+							<p id="1">While Of Mice and Men occurs in a very specific time and place, each of the
 							characters can be thought of as symbolizing broader populations. Though the book
 							is not an allegory, and each character can stand alone as simply a character,
 							there’s still something to be gained by looking at each character as representative
 							of their larger group. Here we go.</p>
+						</div>
 					</div>
-				</div>
 
-				<!--row 2-->
-				<div class="row">
-					<div class="col-sm-2 col-md-2 col-sm-offset-2">
-						<div class="thumbnail">
-							<img class="img-circle" src="images/city.jpg" alt="city">
-							<div class="Sacramento">
-								<h3>Downtown</h3>
-								<p>Come to downtown Sacramento! Not really.</p>
+					<!--row 3-->
+					<div class="row">
+						<div class="col-sm-2 col-md-2 col-sm-offset-2">
+							<div class="thumbnail">
+								<img class="img-circle" src="images/city.jpg" alt="city">
+								<div class="Sacramento">
+									<h3>Downtown</h3>
+									<p>Come to downtown Sacramento! Not really.</p>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col-sm-2 col-md-2">
-						<div class="thumbnail">
-							<img class="img-circle" src="images/tacos.jpg" alt="tacos">
-							<div class="Tacos">
-								<h3>Tacos</h3>
-								<p>Mmmm. Best Tacos.</p>
+						<div class="col-sm-2 col-md-2">
+							<div class="thumbnail">
+								<img class="img-circle" src="images/tacos.jpg" alt="tacos">
+								<div class="Tacos">
+									<h3>Tacos</h3>
+									<p>Mmmm. Best Tacos.</p>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col-sm-2 col-md-2">
-						<div class="thumbnail">
-							<img class="img-circle" src="images/tyson.jpg" alt="tyson">
-							<div class="Mike Tyson">
-								<h3>Mike Tyson</h3>
-								<p>I Love Pigeons!!</p>
+						<div class="col-sm-2 col-md-2">
+							<div class="thumbnail">
+								<img class="img-circle" src="images/tyson.jpg" alt="tyson">
+								<div class="Mike Tyson">
+									<h3>Mike Tyson</h3>
+									<p>I Love Pigeons!!</p>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col-sm-2 col-md-2">
-						<div class="thumbnail">
-							<img class="img-circle" src="images/startrek.jpg" alt="trek">
-							<div class="Star Trek Ship">
-								<h3>Star Trek</h3>
-								<p>Trek Ship. WOW!!</p>
+						<div class="col-sm-2 col-md-2">
+							<div class="thumbnail">
+								<img class="img-circle" src="images/startrek.jpg" alt="trek">
+								<div class="Star Trek Ship">
+									<h3>Star Trek</h3>
+									<p>Trek Ship. WOW!!</p>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</main>
+			</main>
 		</div>
 
 		<footer>
 			<div class="container">
 					<div class="row">
-						<div class="col-md-6 col-md-offset-5">
+						<div class="col-md-6">
+							&copy; 2017 Roza Productions 2017
+						</div>
+						<div class="col-md-6">
 							&copy; 2017 Roza Productions 2017
 						</div>
 					</div>
