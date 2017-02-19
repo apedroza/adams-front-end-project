4eef1ab3f04c7439f22b530418a91ffa6c8d4465
Adam Pedroza <apedroza6@cnm.edu>
2017-02-18 19:12:52 -0700

    added js to change p1 color and h1 header font style both on click

--- public_html/index.php
+++ public_html/index.php
@@ -36,7 +36,7 @@
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 							</button>
-							<a class="navbar-brand" href="#">Ice Cold Marketing <i class="fa fa-hand-o-right" aria-hidden="true"></i></a>
+							<a class="navbar-brand" href="#">Ice Cold Marketing<i class="fa fa-hand-o-right" aria-hidden="true"></i></a>
 						</div>
 
 						<!-- Collect the nav links, forms, and other content for toggling -->
